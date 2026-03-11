4a4d8d566ec044539b5e127938c62f62c8081e16
RubyGupta-tech <webserviesbygupta@gmail.com>
2026-03-10 20:13:40 -0700

    Fix logo background removal to eliminate mesh artifact and make true transparent sticker

--- make_transparent.py
+++ make_transparent.py
@@ -1,24 +1,27 @@
 from PIL import Image
 
-def remove_background(input_path, output_path):
+def remove_background_better(input_path, output_path):
+    # Open the generated original logo (Option 1 has a white background)
     img = Image.open(input_path).convert("RGBA")
     datas = img.getdata()
     
+    # We want to remove the WHITE background.
+    # Option 1 had a solid white background (#FFFFFF) with the gold/navy logo in the center.
     newData = []
-    # Dark navy blue background from generation is roughly #0a2540 (R:10, G:37, B:64)
-    # We'll remove pixels that are very dark blue/black
+    
     for item in datas:
-        # Check if pixel is dark (r<40, g<60, b<90)
-        if item[0] < 40 and item[1] < 60 and item[2] < 90:
-            newData.append((255, 255, 255, 0)) # Make transparent
+        # Check if pixel is white or very close to white (background)
+        # We will make pixels transparent if they are bright white
+        if item[0] > 240 and item[1] > 240 and item[2] > 240:
+            newData.append((255, 255, 255, 0)) # Fully transparent
         else:
             newData.append(item)
             
     img.putdata(newData)
     img.save(output_path, "PNG")
-    print(f"Saved transparent logo to {output_path}")
+    print(f"Saved better transparent logo to {output_path}")
 
-img_in = r"C:\Users\ruby4\.gemini\antigravity\brain\09ee67ff-bd21-4714-8923-9a5a938c83c1\qlw_logo_transparent_1773198525916.png"
+img_in = r"C:\Users\ruby4\New folder\Quantumleapwealth\images\QWL_logo_original.png"
 img_out = r"C:\Users\ruby4\New folder\Quantumleapwealth\images\QWL_logo.png"
 
-remove_background(img_in, img_out)
+remove_background_better(img_in, img_out)
